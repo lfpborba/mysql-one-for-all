6889deaf10a030150aa0f2692b8e32c481bf4826
lfpborba <luisfpb@icloud.com>
2022-07-26 12:25:10 -0300

    Requirement 4 - Done

---
+++ desafio4.sql
@@ -0,0 +1,8 @@
+SELECT
+  u.usuario AS usuario,
+  IF(MAX(YEAR(h.data_reproducao) = 2021), "Usuário Ativo", 'Usuário Inativo') AS condicao_usuario
+FROM SpotifyClone.usuarios AS u
+INNER JOIN SpotifyClone.historico AS h
+ON u.usuario_id = h.usuario_id
+GROUP BY u.usuario
+ORDER BY u.usuario;
